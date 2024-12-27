86ed7c04ab88d799a3d0073c4fad7f77a921b075
tinbit25 <tinbitelias25@gmail.com>
2024-12-27 14:52:58 +0300

    final

--- Book.java
+++ Book.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Book {
     String title;
     String author;
@@ -5,22 +6,32 @@ public class Book {
 Book(String Btitle,String Bauthor){
     title = Btitle;
     author = Bauthor;
-    System.out.println("My book title is " + title);
-    System.out.println("My book author is " + author);
-    
+    System.out.println("The book title is " + title);
+    System.out.println("The book author is " + author);   
 }
 Book(String Btitle,String Bauthor,String Bprice){
     title = Btitle;
     author = Bauthor;
     price = Bprice;
 
-    System.out.println("My book title is " + title);
-    System.out.println("My book author is " + author);
-    System.out.println("My book price is " + price);
+    System.out.println("The book title is " + title);
+    System.out.println("The book author is " + author);
+    System.out.println("The book price is " + price);
 }
 public static void main(String args[]){
-    Book myBook1 = new Book("fikr eske mekabr","hadis alemayehu");
-    Book myBook2 = new Book("fikr eske mekabr","hadis alemayehu","$50");
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the title of the book");
+    String title = input.nextLine();
+    System.out.println("Enter the author of the book");
+    String author = input.nextLine();
+    System.out.println("Enter the price of the book");
+    String price = input.nextLine();
+    Book TheBook = new Book(title,author,price);
+    System.out.println("Enter the title of the book");
+    String title2 = input.nextLine();
+    System.out.println("Enter the author of the book");
+    String author2 = input.nextLine();
+    Book TheBook2 = new Book(title2,author2);
     
 }
 }
