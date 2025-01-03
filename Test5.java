b5e162cd9aa8a1687d9d331a234c8cf3fe194d74
tinbit25 <tinbitelias25@gmail.com>
2025-01-03 11:08:56 +0300

    aadd

---
+++ Test5.java
@@ -0,0 +1,21 @@
+
+class Employee {
+    
+}
+class Programmer extends Employee{
+
+}
+class Manager extends Employee{
+
+}
+class Clerk extends Employee{
+
+}
+public class Test5{
+    public static void main(String[] args) {
+        Employee e = new Employee();
+        Programmer p = new Programmer();
+        Manager m = new Manager();
+        Clerk c = new Clerk();
+    }
+}
