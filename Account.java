fdb61284b89ac615bc094c6d48ef4d7e439fc841
tinbit25 <tinbitelias25@gmail.com>
2025-01-03 09:22:37 +0300

    debug

--- Account.java
+++ Account.java
@@ -1,28 +1,36 @@
 import java.util.Scanner;
+
 public class Account {
     String accountNumber;
     int balance;
-    Account(String accountNumber,int balance){
+
+    Account(String accountNumber, int balance) {
         this.accountNumber = accountNumber;
         this.balance = balance;
     }
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter the account number");
         String accountNumber = sc.nextLine();
-        if(accountNumber.length() == 0){
+
+        if (accountNumber.length() == 0) {
             System.out.println("Invalid account number");
             System.exit(0);
         }
-        if(balance < 0){
+
+        System.out.println("Enter the balance");
+        int balance = sc.nextInt();
+
+        if (balance < 0) {
             System.out.println("Invalid balance");
             System.exit(0);
         }
-        System.out.println("Enter the balance");
-        int balance = sc.nextInt();
-        Account myAccount = new Account(accountNumber,balance);
+
+        Account myAccount = new Account(accountNumber, balance);
+
         System.out.println("My account number is " + myAccount.accountNumber);
         System.out.println("My account balance is " + myAccount.balance);
     }
-    
 }
